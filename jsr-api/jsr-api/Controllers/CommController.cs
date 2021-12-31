fca6a014e6eb6bf634a28525fc2fab45eeea634e
Rahul <rahul.pandya@rws.com>
2021-12-31 19:23:22 +0530

    comm

---
+++ jsr-api/jsr-api/Controllers/CommController.cs
@@ -0,0 +1,39 @@
+﻿using jsr_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jsr_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommController : ControllerBase
+    {
+        [HttpGet("GetData")]
+        public IActionResult GetData()
+        {
+           
+            return Ok();
+        }
+        [HttpPost("Save")]
+        public IActionResult Save([FromBody] ObjClass req)
+        {
+            return Ok();
+        }
+        [HttpPut("Chng")]
+        public IActionResult Chng([FromBody] ObjClass req)
+        {
+            return Ok();
+        }
+        [HttpDelete("Rmv/{Id}")]
+        public IActionResult Rmv(int Id)
+        {
+            return Ok();
+        }
+
+       
+    }
+}
---
+++ jsr-api/jsr-api/Models/ObjClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jsr_api.Models
+{
+    public class ObjClass
+    {
+        public string p1 { get; set; }
+        public string p2 { get; set; }
+        public string p3 { get; set; }
+        public string p4 { get; set; }
+        public string p5 { get; set; }
+
+
+
+    }
+}
